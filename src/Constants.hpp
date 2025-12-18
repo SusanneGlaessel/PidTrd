60e3be1cbb033d1d97394b2bea1bb9c10f591b6b
Susanne Glassel <glaessel@ikf.uni-frankfurt.de>
2025-12-18 16:03:55 +0100

    format change

--- src/Constants.hpp
+++ src/Constants.hpp
@@ -42,11 +42,32 @@ constexpr int NbinsMom = 60;
 constexpr float BwdEdx = 0.1;
 constexpr int NbinsdEdx = 35;
 
-inline std::array<std::pair<Int_t, TString>, NumberOfPidsTrd> pid_codes_trd_{make_pair(2212, "p"), make_pair(211, "pi"), make_pair(321, "K"), make_pair(1000010020, "d"), make_pair(1000010030, "t"), make_pair(1000020030, "he3"), make_pair(1000020040, "he4"), make_pair(11, "e"), make_pair(13, "mu"), make_pair(1, "bg")};
+inline std::array<std::pair<Int_t, TString>, NumberOfPidsTrd> pid_codes_trd_{
+    make_pair(2212, "p"),
+    make_pair(211, "pi"),
+    make_pair(321, "K"),
+    make_pair(1000010020, "d"),
+    make_pair(1000010030, "t"),
+    make_pair(1000020030, "he3"),
+    make_pair(1000020040, "he4"),
+    make_pair(11, "e"),
+    make_pair(13, "mu"),
+    make_pair(1, "bg")
+};
 
 inline TString dirname_tracks_ = "dEdx_tracks";
 inline TString dirname_hits_ = "dEdx_hits";
-inline std::array<TString, NumberOfTrdLayers> dirname_nhits_ = {"1hit", "2hits", "3hits", "4hits"};
-inline std::array<TString, NumberOfTruncMode> histtitle_mode_ = {"truncation mode: 1 hit", "truncation mode: 2 hits", "truncation mode: 3 hits", "truncation mode: 4 hits"};
+inline std::array<TString, NumberOfTrdLayers> dirname_nhits_ = {
+  "1hit",
+  "2hits",
+  "3hits",
+  "4hits"
+};
+inline std::array<TString, NumberOfTruncMode> histtitle_mode_ = {
+  "truncation mode: 1 hit",
+  "truncation mode: 2 hits",
+  "truncation mode: 3 hits",
+  "truncation mode: 4 hits"
+};
 
 #endif// Constants_H
